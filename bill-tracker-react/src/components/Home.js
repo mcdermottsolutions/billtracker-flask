368ccc47663c1564fb7bf97933cabdb4c0d16b2b
b b <bboutette@gmail.com>
2017-05-27 18:32:49 -0500

    started react app.  views not working quite right yet.

---
+++ bill-tracker-react/src/components/Home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+
+render(){
+
+  return(
+    <div> I am the home view </div>
+    );
+  }
+
+}
+
+export default Home;
---
+++ bill-tracker-react/src/components/ResultItem.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+
+
+
+class ResultItem extends Component {
+
+
+
+
+  render(){
+    return(
+
+    <li>I am an individual bill in a list of bill results.</li>
+  )
+  }
+}
+
+export default ResultItem;
