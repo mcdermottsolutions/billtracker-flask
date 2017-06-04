cc9ae3388cefe1f2d6e601a84e0ee54c95d7ef7b
b b <bboutette@gmail.com>
2017-06-04 02:16:40 -0500

    took out some extra code used for testing routes

--- bill-tracker-react-routerv4/src/components/About.js
+++ bill-tracker-react-routerv4/src/components/About.js
@@ -8,15 +8,6 @@ class About extends Component {
       <div>
         I am the about page.
 
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          {this.props.children}
-          <li><Link to='/about'>About</Link></li>
-
-          {this.props.children}
-          <li><Link to='/contact'>Contact Us</Link></li>
-          <li><Link to='/demo'>Demo</Link></li>
-        </ul>
       </div>
     );
   }
